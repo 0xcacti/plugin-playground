9f92ca15dd9c3191670c787eebdc970f663c5a40
0xcacti <0xcacti@gmail.com>
2023-09-22 01:04:17 -0400

    begin playing around

--- lua/banner/init.lua
+++ lua/banner/init.lua
@@ -1,1 +1,44 @@
 local M = {}
+
+M.setup = function()
+    print("penis")
+end
+
+local find_mapping = function(maps, lhs)
+    for _, value in ipairs(maps) do
+        if value.lhs == lhs then
+            return value
+        end
+    end
+end
+
+M._stack = {}
+
+M.push = function(name, mode, mappings)
+    local maps = vim.api.nvim_get_keymap(mode)
+    local existing_maps = {}
+    for lhs, rhs in pairs(mappings) do
+        local existing = find_mapping(maps, lhs)
+        if existing then
+            table.insert(existing_maps, existing)
+        end
+    end
+    P(existing_maps)
+    M._stack = existing_maps
+    for lhs, rhs in pairs(mappings) do
+        vim.keymap.set(mode, lhs, rhs)
+    end
+end
+
+M.push("debug_mode", "n", {
+    [" st"] = "echo 'Hello'",
+    [" sz"] = "echo 'Hello'",
+})
+
+M.pop = function(name)
+end
+
+return M
+
+
+--
