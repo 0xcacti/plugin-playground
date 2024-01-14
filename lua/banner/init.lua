e628ad982178b066fc91f62e5292249f836289a0
0xcacti <0xcacti@gmail.com>
2024-01-14 03:18:49 -0500

    save

--- lua/banner/init.lua
+++ lua/banner/init.lua
@@ -1,63 +1,24 @@
 local M = {}
 
-
-local find_mapping = function(maps, lhs)
-    for _, value in ipairs(maps) do
-        if value.lhs == lhs then
-            return value
-        end
-    end
-end
-
 M._stack = {}
 
-M.push = function(name, mode, mappings)
-    local maps = vim.api.nvim_get_keymap(mode)
-
-    local existing_maps = {}
-    for lhs, rhs in pairs(mappings) do
-        local existing = find_mapping(maps, lhs)
-        if existing then
-            existing_maps[lhs] = existing
-        end
-    end
-
-    for lhs, rhs in pairs(mappings) do
-        -- TODDO: need some way to pass options in here
-        vim.keymap.set(mode, lhs, rhs)
-    end
-
-    -- TODO: Next time show bash about metatables POGSLIDE
-    M._stack[name] = M._stack[name] or {}
-
-    M._stack[name][mode] = {
-        existing = existing_maps,
-        mappings = mappings,
-    }
+M.comment = function()
+    local line = vim.api.nvim_get_current_line()
+    local comment_line = "-- " .. line
+    vim.api.nvim_set_current_line(comment_line)
 end
 
-M.pop = function(name, mode)
-    local state = M._stack[name][mode]
-    M._stack[name][mode] = nil
-
-    for lhs in pairs(state.mappings) do
-        if state.existing[lhs] then
-            -- Handle mappings that existed
-            local og_mapping = state.existing[lhs]
-
-            -- TODO: Handle the options from the table
-            vim.keymap.set(mode, lhs, og_mapping.rhs)
-        else
-            -- Handled mappings that didn't exist
-            vim.keymap.del(mode, lhs)
-        end
-    end
+M.uncomment = function()
+    local line = vim.api.nvim_get_current_line()
+    local comment_line = line:sub(4)
+    vim.api.nvim_set_current_line(comment_line)
 end
 
 M.clear = function()
     M._stack = {}
 end
+
+vim.api.nvim_create_user_command('CommentLine', M.comment, {})
+vim.api.nvim_create_user_command('UncommentLine', M.uncomment, {})
+
 return M
-
-
---
