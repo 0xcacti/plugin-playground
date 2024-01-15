96b53dc4b362593dc7f460b42b09cf5046e51a5c
0xcacti <0xcacti@gmail.com>
2024-01-15 02:52:10 -0500

    save

--- lua/banner/init.lua
+++ lua/banner/init.lua
@@ -1,31 +1,31 @@
 local M = {}
+local static = require('static')
+local utils = require('utils')
+local core = require('core')
 
-M._stack = {}
 
-M.comment = function()
-    local line = vim.api.nvim_get_current_line()
-    local position = string.find(line, "%S")
-    local comment_line
-    if position then
-        comment_line = line:sub(1, position - 1) .. "-- " .. line:sub(position)
-    else
-        comment_line = "-- " .. line
-    end
-    vim.api.nvim_set_current_line(comment_line)
+M.setup = function(new_config)
+
 end
 
-M.uncomment = function()
-    local line = vim.api.nvim_get_current_line()
 
-    local comment_line = line:sub(4)
-    vim.api.nvim_set_current_line(comment_line)
+local M.check_log = function()
+    local bufnr = vim.api.nvim_create_buf(false,true)
+    core.win.proportional_size( static.config.win.width_ratio, static.config.win.height_ratio )
+
+    -- check the logs
+    local file_name = vim.api.nvim_buf_get_name(0)
+    local line_range = utils.range()
+    local extra_args = core.lua.list.reduce(static.config.extra_args, function(acc, arg)
+        return acc .. " " .. arg
+    end, "")
+    local log = vim.api.nvim_exec2(
+        string.format("!git log %s -L %s,%s,%s", extra_args, line_range[1], line_range[2], file_name),
+        { output = true }
+    )
+
+    -- format log
+
 end
-
-M.clear = function()
-    M._stack = {}
-end
-
-vim.api.nvim_create_user_command('CommentLine', M.comment, {})
-vim.api.nvim_create_user_command('UncommentLine', M.uncomment, {})
 
 return M
